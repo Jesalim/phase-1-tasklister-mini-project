72b3a6bbc2fd98423453d5a4954706c22b79e9da
Jesalim <jessyculture@gmail.com>
2022-12-09 14:15:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelector('form').addEventListener('submit', (e)=>
+  {
+    e.preventDefault();
+  
+  const todo = e.target.fname.value;
+  const p = document.createElement('li');
+  const btn = document.createElement('button');
+  btn.textContent = 'x';
+  p.textContent = todo;
+  let n = document.querySelector('ul').append(p,btn);
+  btn.addEventListener('click', ()=>
+  {
+    p.remove()
+    btn.remove()
+  })
+  })
+ 
+
 });
